9a6c871b7e93bfe2dcc3fa9d5fd0a150b461f720
Cesar Zuñiga <cesar.zuniga@mobiik.com>
2023-11-15 16:07:20 -0600

    add jquery on main

--- demo-summmernote-editor/src/main.js
+++ demo-summmernote-editor/src/main.js
@@ -1,5 +1,9 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue';
+import * as jQuery from "jquery";
+
+// define & and jQuery on the global window object
+Object.assign(window, { $: jQuery, jQuery });
 createApp(App).mount('#app')
