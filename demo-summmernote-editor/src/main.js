8f80013c812dc5d4d25b29b3da8f3f480e307106
Cesar Zuñiga <cesar.zuniga@mobiik.com>
2023-11-16 13:05:31 -0600

    change imports

--- demo-summmernote-editor/src/main.js
+++ demo-summmernote-editor/src/main.js
@@ -2,8 +2,4 @@ import './assets/main.css'
 
 import { createApp } from 'vue';
 import App from './App.vue';
-import * as jQuery from "jquery";
-
-// define & and jQuery on the global window object
-Object.assign(window, { $: jQuery, jQuery });
 createApp(App).mount('#app')
